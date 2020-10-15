9b95244fe7b44f534d531c5cbbcab050a00ec2a0
Alexey KOLESAEV <kolesaev@AGRANA.NET>
2020-10-15 19:23:47 +0300

    Added project

---
+++ BlazorApp1/Data/Global.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace BlazorApp1.Data
+{
+    public class Global
+    {
+        public static IConfiguration configuration { get; set; }
+    }
+}
